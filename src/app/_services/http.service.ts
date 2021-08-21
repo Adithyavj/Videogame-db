803dd22b3a64daed68772b911f274afd4fc2fc90
Adithya Vijay K <adithyavj@gmail.com>
2021-08-21 20:25:53 +0530

    Added http service for get api endpoint

---
+++ src/app/_services/http.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
+import { APIResponse } from '../_models/apiResponse';
+import { Game } from '../_models/game';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getGameList(ordering: string, search?: string): Observable<APIResponse<Game>> {
+
+    // setting the parameters in url string
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params
+    });
+  }
+}
