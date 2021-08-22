4f4866886d7e118a5a70b0ce697cfebf6815979a
Adithya Vijay K <adithyavj@gmail.com>
2021-08-22 19:52:12 +0530

    Populate home page with data from api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse } from 'src/app/_models/apiResponse';
+import { Game } from 'src/app/_models/game';
+import { HttpService } from 'src/app/_services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public sort: string;
+  public games: Array<Game> = new Array<Game>();
 
-  constructor() { }
+  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // when the home page loads, we want to check if there are any params.
+    // if yes check the type of param
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    // call the method in service to getGameList and add the returned value to games array
+    this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+      this.games = gameList.results;
+      console.log(this.games);
+    });
   }
 
 }
