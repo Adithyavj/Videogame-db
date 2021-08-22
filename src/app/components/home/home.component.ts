fa09f777d6d67f525bf72957753cc94319aaf30d
Adithya Vijay K <adithyavj@gmail.com>
2021-08-22 20:05:46 +0530

    Avoid memory leaks by using ngDestory method and unsubscribing from games and route subscriptions.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse } from 'src/app/_models/apiResponse';
 import { Game } from 'src/app/_models/game';
 import { HttpService } from 'src/app/_services/http.service';
@@ -9,17 +10,19 @@ import { HttpService } from 'src/app/_services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   public sort: string;
   public games: Array<Game> = new Array<Game>();
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     // when the home page loads, we want to check if there are any params.
     // if yes check the type of param
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -30,7 +33,7 @@ export class HomeComponent implements OnInit {
 
   searchGames(sort: string, search?: string): void {
     // call the method in service to getGameList and add the returned value to games array
-    this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+    this.gameSub = this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
       this.games = gameList.results;
       console.log(this.games);
     });
@@ -39,4 +42,16 @@ export class HomeComponent implements OnInit {
   openGameDetails(id: string) {
     this.router.navigate(['details', id]);
   }
+
+  // implement ngOnDestroy interface
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+
 }
